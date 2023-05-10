1d1b4798d4ba6df33278d4114327667e38d073bb
EHTESHAM855 <105726451+EHTESHAM855@users.noreply.github.com>
2023-05-10 12:51:28 +0530

    SOME CHANGES

---
+++ script3.js
@@ -0,0 +1,9 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzz882CZVXIzAJ2D-cUhERjnhLYtXRzo5gk5i1i0W8luWSSZSgXVzaoYVQy8Mz1DEJjqA/exec'
+            const form = document.forms['Ehtesham-eXPENSE']
+          
+            form.addEventListener('submit', e => {
+              e.preventDefault()
+              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => alert("Succesfully Recorded..."))
+                .catch(error => console.error('Error!', error.message))
+            })
